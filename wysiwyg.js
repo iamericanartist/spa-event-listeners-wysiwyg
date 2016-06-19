685d444f3b8530bacfad523c7e61829406c8cb38
Eric Denton <eric@iamericanartist.com>
2016-06-19 17:43:08 -0500

    Submitting progress, nearly complete

--- wysiwyg.js
+++ wysiwyg.js
@@ -12,8 +12,7 @@ var bluthMember = [
       birth: 1970,
       death: "n/a"
     }
-  },
-    {
+  },{
     title: "Magician",
     name: "George Oscar Bluth",
     bio: "G.O.B. (short for George Oscar Bluth) is the oldest of the five Bluth children. Named after his father, G.O.B. inherited none of George's intelligence or motivation. His greed led him to the life of a magician, among other quick-income professions such as male stripping. After receiving media attention revealing how an illusion worked, G.O.B. was blacklisted from magic and struggled to make his way back into the inner circle. ",
@@ -22,8 +21,7 @@ var bluthMember = [
       birth: 1969,
       death: "n/a"
     }
-  },
-    {
+  },{
     title: "Army (Excused From Service)",
     name: "Buster Bluth",
     bio: "Buster Bluth, real name Byron, is the baby of the Bluth family, despite being in his thirties. He is extremely dependent on his mother Lucille, perhaps because he spent 11 months in her womb. Although raised to believe George was his father, he eventually learned that Oscar Bluth, George's identical twin brother, was having an affair with Lucille and is probably his biological father. Buster was a scholar until the family went broke, and served in Army for a moment until his left hand was bitten off by a loose seal. ",
@@ -32,8 +30,7 @@ var bluthMember = [
       birth: 1973,
       death: "TBD"
     }
-  },
-    {
+  },{
     title: "Bluth Company ",
     name: "Lindsay Bluth Fünke",
     bio: "Lindsay Bluth Fünke, né Nellie, was adopted by the Bluth family at age three and told she was a fraternal twin to Michael. Appreciated only for her looks by her parents, Lindsay began acting out in rebellion, including moving to Boston, marrying Tobias Fünke and hosting charity dinners to spend her time. Lindsay and Tobias had to turn to a doctor to get pregnant but eventually gave birth to Maeby Fünke.",
@@ -42,8 +39,7 @@ var bluthMember = [
       birth: 1971,
       death: "TBD"
     }
-  },
-    {
+  },{
     title: "Bluth Company Founder",
     name: "George Bluth",
     bio: "George Bluth is the patriarch of the Bluth family. After founding the Bluth Company in 1953, George stole his soon-to-be-wife Lucille from his twin brother Oscar and forced him out of the real estate business. In 2003 he was arrested by the SEC for stealing money from his company but was ultimately pardoned by the CIA.",
@@ -52,8 +48,7 @@ var bluthMember = [
       birth: 1944,
       death: "TBD"
     }
-  },
-    {
+  },{
     title: "Bluth Family Matriarch",
     name: "Lucille Bluth",
     bio: "Lucille Bluth is the matriarch, and to a degree the puppet master of the Bluth family (no, not including Franklin). Using insults as a form of parenting, Lucille longs for others to be jealous of her and is a functional alcoholic. She is manipulative and cold, but at times displays warmness and protection for her children. She was arrested after George's pardon for masterminding George's fraud.",
@@ -74,50 +69,61 @@ var updateBioTextEl = document.getElementById("updateBioText"), //basically the 
 // // 3. Beneath that, create a container, block element in your DOM.
 // // 4. Create a DOM element for each of the objects inside the container. Style your person elements however you like.
 var bluthDOMED = document.getElementById("outputArea");
-//add content to DOM
+// //add bluthCards to DOM
 for (var i = 0; i < bluthMember.length; i++) {
   var bluthCard = bluthMember[i];
-
   bluthDOMED.innerHTML += `
-    <person id="bluthCard-${i}" class="bluthCard" onClick="identifyClickedElement()">
+    <person id="bluthCard-${i}" class="bluthCard"">
       <header class="bluthNameTitle">
         <h2>${bluthCard.name}</h2> 
         <h4>${bluthCard.title}</h4>
         <hr>
       </header>
-
       <section>
         <img class="dosierPhoto" src="${bluthCard.image}" alt="${bluthCard.title} ${bluthCard.name}">
         <p id="bluthAbout">${bluthCard.bio}</p>
       </section>
-      
       <footer class="bluthLife">
         <p>Born: ${bluthCard.lifespan.birth} / </p>
         <p>Died: ${bluthCard.lifespan.death}</p>
       </footer>
     </person>
-    `;
-    console.log("<---This many cards");
+  `;
+  // console.log("<---This many cards");
 }
 
 
 // // 5. For every even numbered element, have a light yellow background.
-for (var i = 0; i <= bluthMember.length; i++) {
-  bluthMember[i]
-}
-
+  // CSS: using ".bluthCard:nth-of-type(even)"
 // // 6. For every odd numbered element, have a light blue background.
+  // CSS: using ".bluthCard:nth-of-type(odd)"
 // // 7. Each element's DOM structure should be as shown below.
+  //JS: see string interpolation above (~line 81)
+
+
 // // 8. When you click on one of the person elements, a dotted border should appear around it.
 // // 9. When you click on one of the person elements, the text input should immediately gain focus so that you can start typing.
+var thisBluthCard = document.getElementsByClassName("bluthCard");
+for (var i = 0; i < thisBluthCard.length; i++) {
+  thisBluthCard[i].addEventListener("click", addBorder);
+}
+function addBorder(ID) {
+  // console.log("Banana Store!");
+  for (var j = 0; j < thisBluthCard.length; j++){
+    thisBluthCard[j].classList.remove("borderTown");
+  }
+  thisBluthCard[this.id].classList.add("borderTown");
+  document.getElementById("updateBioText").focus();
+  console.log("This Card is ", thisBluthCard[this.id]);
+}
+
+
 // // 10. When there is a highlighted person element, and you begin typing in the input box, the person's biography should be immediately bound to what you are typing, letter by letter.
-
-
-// for 
 updateBioTextEl.addEventListener("keyup", (function(event) {
   bluthAbout.innerHTML = updateBioTextEl.value;
   // console.log("on keyup =", keypressInputEl.value);
 }));
+
 
 
 // // 11. When you press the enter/return key when typing in the input field, then the content of the input field should immediately be blank.
@@ -128,10 +134,8 @@ function ifEnterKey () {
   updateBioTextEl.value = "";
   // console.log(">>Text added, input cleared √", updateBioTextEl.value);
   } 
-  // else {  //only used to check key press values
-  //   console.log(event.which," was Pushed");
-  // }
 }
 
+// // EVENT LISTENERS
 // updateBioTextEl.addEventListener("keyup", ifEnterKey) // ALL Key listener on text field
 updateBioTextEl.addEventListener("keyup", ifEnterKey) // ENTER Key listener on text field
